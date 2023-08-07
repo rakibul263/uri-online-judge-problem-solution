e1f57079dbe1eec3f27bfa3533ab270b9c720c12
Md Rakibul Hasan <121483344+rakibul263@users.noreply.github.com>
2023-08-08 00:14:09 +0600

    Add files via upload

---
+++ 1930_Electrical Outlet.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int T1,T2,T3,T4;
+    scanf("%d %d %d %d",&T1, &T2, &T3, &T4);
+    int result = (T1+T2+T3+T4)-3;
+    printf("%d\n",result);
+
+    return 0;
+}
---
+++ 1930_Electrical Outlet.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int T1,T2,T3,T4;
+    cin>>T1>>T2>>T3>>T4;
+    int result = (T1+T2+T3+T4)-3;
+    cout<<result<<endl;
+
+    return 0;
+}
